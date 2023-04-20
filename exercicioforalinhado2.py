d336d513a21f1f29815619b0a43d2ff0b525f6b1
Messias-Acacy <130923301+Messias-Acacy@users.noreply.github.com>
2023-04-19 21:37:09 -0300

    Add files via upload

---
+++ exercicioforalinhado2.py
@@ -0,0 +1,12 @@
+#for x in range(10):
+#    for y in range (10):
+#        print(y, end='')
+#        print(y, end=' ')
+#    print()
+
+#for x in range(10):
+#    for y in range (9,-1,-1):
+#        print(y, end='')
+#        print(y, end=' ')
+#   print()
+
