f47e18ea230c38c571cc196f96c99289c861a16c
Messias-Acacy <130923301+Messias-Acacy@users.noreply.github.com>
2023-04-27 20:33:02 -0300

    Add files via upload

---
+++ aula7.1.py
@@ -0,0 +1,16 @@
+for x in range(3):
+    for y in range(x):
+        if y==1:
+            print('   ', end=' ')
+            print('*')
+for x in range(3):
+    for y in range(x):
+        if y==1:
+            print(' ','*',' ','*')
+for x in range(3):
+    for y in range(x):
+        if y == 1:
+            print(' ', end='   ')
+            print('*')
+            
+
